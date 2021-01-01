288034df778da1f52f1785da2890f1040efdd204
BroJoon <ydngjink1234@gmail.com>
2021-01-02 06:15:32 +0900

    strnstr fix2

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,33 +6,24 @@
 /*   By: hyungjki <hyungjki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 10:42:15 by hyungjki          #+#    #+#             */
-/*   Updated: 2021/01/02 05:56:34 by hyungjki         ###   ########.fr       */
+/*   Updated: 2021/01/02 06:15:10 by hyungjki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t size)
 {
-	size_t	idx;
-	size_t	cur;
+	size_t len;
 
-	if (!little)
+	if (*little == '\0')
 		return (char *)big;
-	if (!big || len == 0)
-		return (NULL);
-	while ((cur < len) && big + cur)
+	len = ft_strlen(little);
+	while (*big && size-- >= len)
 	{
-		idx = 0;
-		while (idx < ft_strlen(little) && idx + cur < len)
-		{
-			if (big[cur + idx] != little[idx])
-				break ;
-			idx++;
-		}
-		if (idx == ft_strlen(little))
-			return ((char *)(big + cur));
-		cur++;
+		if (*big == *little && ft_strncmp(big, little, len) == 0)
+			return (char*)big;
+		big++;
 	}
 	return (0);
 }
