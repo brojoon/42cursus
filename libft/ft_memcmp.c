af142be5e5dbfcf60d2f9036631968d3dc59acb7
BroJoon <ydngjink1234@gmail.com>
2021-01-03 01:02:34 +0900

    wefew

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,25 +6,21 @@
 /*   By: hyungjki <hyungjki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 07:51:02 by hyungjki          #+#    #+#             */
-/*   Updated: 2021/01/03 00:47:48 by hyungjki         ###   ########.fr       */
+/*   Updated: 2021/01/03 01:02:26 by hyungjki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char *ptr1;
+	unsigned char *ptt1;
 	unsigned char *ptr2;
 
-	ptr1 = (unsigned char *)s1;
+	ptt1 = (unsigned char *)s1;
 	ptr2 = (unsigned char *)s2;
 	while (n--)
-	{
-		if (*ptr1 != *ptr2)
-			return (ptr1 - ptr2);
-		ptr1++;
-		ptr2++;
-	}
+		if (*ptt1++ != *ptr2++)
+			return ((ptt1 - 1) - (ptr2 - 1));
 	return (0);
 }
--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,34 +6,36 @@
 /*   By: hyungjki <hyungjki@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 21:10:38 by hyungjki          #+#    #+#             */
-/*   Updated: 2021/01/03 00:51:28 by hyungjki         ###   ########.fr       */
+/*   Updated: 2021/01/03 01:02:20 by hyungjki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t destsize)
 {
-	size_t cur;
-	size_t dest_len;
-	size_t src_len;
+	size_t		i;
+	size_t		j;
+	size_t		k;
+	size_t		l;
 
-	src_len = ft_strlen(src);
-	dest_len = ft_strlen(dest);
-	if (src_len + 1 < destsize)
+	k = 0;
+	while (src[k])
+		k++;
+	if (destsize == 0)
+		return (k);
+	i = 0;
+	while (dst[i] && i < destsize)
+		i++;
+	l = i;
+	j = 0;
+	if (i < destsize)
 	{
-		while (*dest)
-			dest++;
-		cur = 0;
-		while ((cur < destsize - src_len - 1) && src[cur])
-		{
-			*(dest) = src[cur];
-			dest++;
-			cur++;
-		}
-		*dest = '\0';
+		while (i < (destsize - 1) && src[j])
+			dst[i++] = src[j++];
+		dst[i] = '\0';
 	}
-	if (src_len > destsize)
-		return (destsize + dest_len);
-	return (src_len + dest_len);
+	if (destsize - 1 < l)
+		return (destsize + k);
+	return (k + l);
 }
